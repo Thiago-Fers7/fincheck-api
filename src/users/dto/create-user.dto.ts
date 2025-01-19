47d19a09ba33d64458f357d0e0ff17e27cddb40f
Luiz Thiago <thiago_fers7@hotmail.com>
2025-01-19 12:17:37 -0300

    adiciona validação de dados no DTO de criação de usuário e implementa serviço Prisma para gerenciamento de usuários

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsNotEmpty({ message: 'O nome não pode estar vazio' })
+  name: string;
+
+  @IsString({ message: 'O email deve ser uma string' })
+  @IsNotEmpty({ message: 'O email não pode estar vazio' })
+  @IsEmail({}, { message: 'O email deve ser um endereço de email válido' })
+  email: string;
+
+  @IsString({ message: 'A senha deve ser uma string' })
+  @IsNotEmpty({ message: 'A senha não pode estar vazia' })
+  @MinLength(8, { message: 'A senha deve ter pelo menos 8 caracteres' })
+  password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    console.log('createUserDto:', createUserDto);
-    return 'This action adds a new user';
+  constructor(private readonly primsmaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const userAlreadyExists = await this.primsmaService.user.findUnique({
+      where: { email },
+    });
+
+    if (userAlreadyExists) {
+      throw new ConflictException('Email já cadastrado');
+    }
+
+    const user = await this.primsmaService.user.create({
+      data: { name, email, password },
+    });
+
+    return user;
   }
 }
